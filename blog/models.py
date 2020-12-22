5d42e1b533d6fb7d324d5a921670aca381cf5861
Syed Ali <syedali3267@gmail.com>
2020-12-22 15:11:49 +0000

    Added auto date

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from datetime import datetime, date
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(
+       'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
