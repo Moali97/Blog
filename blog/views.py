b927bc20db4e7382df0595f19bfcc1ba170ec743
Syed Ali <syedali3267@gmail.com>
2020-12-23 15:17:51 +0000

    Added sepaarte blog posts and test

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 class BlogListView(ListView):
 
     model = Post
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
