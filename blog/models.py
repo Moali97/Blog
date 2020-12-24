1d017d0363c4bf7ae02fcf5086152de76b7e71e3
Syed Ali <syedali3267@gmail.com>
2020-12-24 15:25:01 +0000

    Added genre of blog

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,25 @@ from django.db import models
 from datetime import datetime, date
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.ForeignKey(
-       'auth.User',
-        on_delete=models.CASCADE,
+    SUBJECT_CHOICES = (
+        ('tech', 'Technology'),
+        ('finance', 'Financial'),
+        ('cons', 'Consumer'),
+        ('cyber', 'Cybersecurity'),
     )
+    genre = models.CharField(max_length=25, choices=SUBJECT_CHOICES, default='DEFAULT VALUE')
+
+    title = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30, default='DEFAULT VALUE')
+    last_name = models.CharField(max_length=30, default='DEFAULT VALUE')
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
 
+
+
+
+
+
     def __str__(self):
         return self.title
+
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,11 @@ from .models import Post
 from django.views.generic import ListView, DetailView
 
 class BlogListView(ListView):
-
     model = Post
     template_name = 'home.html'
 
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
