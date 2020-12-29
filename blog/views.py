617a8a988f5a5be17e291dcbcf6ba8602154d1bf
Syed Ali <syedali3267@gmail.com>
2020-12-29 17:29:58 +0000

    Added sign-in and sign-out buttons

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,32 @@
-from django.shortcuts import render
-from django.views.generic import ListView
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+
 class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['title', 'first_name', 'last_name', 'body','genre' ]
+    fields = ['title', 'first_name', 'last_name', 'body', 'genre']
 
 
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body', 'genre']
 
 
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
