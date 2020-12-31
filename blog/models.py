75eb93004b52d1430314f02de8a2f88ad0825df8
Syed Ali <syedali3267@gmail.com>
2020-12-31 18:53:55 +0000

    Added User Authorisation

--- blog/models.py
+++ blog/models.py
@@ -26,17 +26,3 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
 
-
-class Entry(models.Model):
-    """Something specific learned about a topic."""
-    topic = models.ForeignKey(Post, on_delete=models.CASCADE)
-    text = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = 'entries'
-
-
-    def __str__(self):
-        """Return a string representation of the model."""
-        return f"{self.text[:50]}..."
--- blog/views.py
+++ blog/views.py
@@ -2,31 +2,37 @@ from .models import Post
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'home.html'
+    login_url = 'login'
 
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
+    login_url = 'login'
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'first_name', 'last_name', 'body', 'genre']
+    login_url = 'login'
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body', 'genre']
+    login_url = 'login'
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
+    login_url = 'login'
